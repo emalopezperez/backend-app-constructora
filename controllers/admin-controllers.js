5b14488af02099d6c3c679253beccad57f3d9072
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-10-29 12:03:18 -0300

    Added solution for cookie issues in production deployment

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -20,6 +20,7 @@ const loginAdmin = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "None",
       });
 
       const dataAdmin = {
