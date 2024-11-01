7de98f8ee609082732f3f8556fb9d3598c8e5116
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-11-01 08:13:34 -0300

    c3

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PUT", "HEAD"],
     allowedHeaders: [
